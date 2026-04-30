b7c1cbfe1a7921873060091559354ee7fc04c9ec
Paweł Chochura <pchochura@gmail.com>
2026-04-30 09:12:38 +0200

    Feat: Implement account removal redirection on iOS
    - Added `openRemoveAccountWebsite` implementation to `IosAppRepository`
    - Updated DI to inject the removal website URL from BuildKonfig

--- core/src/iosMain/kotlin/com/pointlessapps/songbook/core/app/IosAppRepository.kt
+++ core/src/iosMain/kotlin/com/pointlessapps/songbook/core/app/IosAppRepository.kt
@@ -4,11 +4,28 @@ import platform.Foundation.NSURL
 import platform.UIKit.UIApplication
 import platform.UIKit.UIApplicationOpenSettingsURLString
 
-internal class IosAppRepository : AppRepository {
+internal class IosAppRepository(
+    private val removeAccountUrl: String,
+) : AppRepository {
     override fun openAppSettings() {
         val settingsUrl = NSURL(string = UIApplicationOpenSettingsURLString)
         if (UIApplication.sharedApplication.canOpenURL(settingsUrl)) {
             UIApplication.sharedApplication.openURL(settingsUrl)
         }
+    }
+
+    override fun openRemoveAccountWebsite(accessToken: String, refreshToken: String) {
+        val websiteUrl = NSURL(
+            string = "$removeAccountUrl?$ACCESS_TOKEN=$accessToken&$REFRESH_TOKEN=$refreshToken",
+        )
+
+        if (UIApplication.sharedApplication.canOpenURL(websiteUrl)) {
+            UIApplication.sharedApplication.openURL(websiteUrl)
+        }
+    }
+
+    private companion object {
+        const val ACCESS_TOKEN = "access_token"
+        const val REFRESH_TOKEN = "refresh_token"
     }
 }
--- core/src/iosMain/kotlin/com/pointlessapps/songbook/core/app/di/AppModule.ios.kt
+++ core/src/iosMain/kotlin/com/pointlessapps/songbook/core/app/di/AppModule.ios.kt
@@ -1,11 +1,14 @@
 package com.pointlessapps.songbook.core.app.di
 
+import com.pointlessapps.songbook.core.BuildKonfig
 import com.pointlessapps.songbook.core.app.AppRepository
 import com.pointlessapps.songbook.core.app.IosAppRepository
-import org.koin.core.module.dsl.singleOf
-import org.koin.dsl.bind
 import org.koin.dsl.module
 
 internal val appModule = module {
-    singleOf(::IosAppRepository).bind<AppRepository>()
+    single<AppRepository> {
+        IosAppRepository(
+            removeAccountUrl = BuildKonfig.REMOVE_ACCOUNT_WEBSITE_URL.orEmpty(),
+        )
+    }
 }
