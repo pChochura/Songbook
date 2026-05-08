8fb1e587573c27b5d0ecbea8d883a851a8832141
Paweł Chochura <pchochura@gmail.com>
2026-05-08 12:02:59 +0200

    Refactor: Update Google Auth to use GetSignInWithGoogleOption
    - Replaces GetGoogleIdOption with GetSignInWithGoogleOption
    - Simplifies the credential request logic in AndroidGoogleAuthManager

--- core/src/androidMain/kotlin/com/pointlessapps/songbook/core/auth/AndroidGoogleAuthManager.kt
+++ core/src/androidMain/kotlin/com/pointlessapps/songbook/core/auth/AndroidGoogleAuthManager.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import androidx.credentials.CredentialManager
 import androidx.credentials.GetCredentialRequest
 import androidx.credentials.exceptions.GetCredentialException
-import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GetSignInWithGoogleOption
 import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -16,14 +16,8 @@ internal class AndroidGoogleAuthManager(
 
     override suspend fun getGoogleTokens(): GoogleTokens? = withContext(Dispatchers.IO) {
         val credentialManager = CredentialManager.create(context)
-
-        val googleIdOption = GetGoogleIdOption.Builder()
-            .setFilterByAuthorizedAccounts(false)
-            .setServerClientId(webClientId)
-            .build()
-
         val request = GetCredentialRequest.Builder()
-            .addCredentialOption(googleIdOption)
+            .addCredentialOption(GetSignInWithGoogleOption.Builder(webClientId).build())
             .build()
 
         try {
